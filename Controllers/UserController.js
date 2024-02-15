acb4cd11b5a906e5443ba55cfe3db8e1c52001ba
SIVUPE <sivuyisiwepeter79@gmail.com>
2024-02-15 16:32:30 +0200

    controller

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -16,3 +16,31 @@ userRouter.get('/',(req, res)=>{
         })
     }
  })
+
+
+userRouter.get('/:id',(req, res)=>{
+    try{
+        users.fetchUsers(req, res)
+    }catch(e) {
+        res.json({
+            status: res.statusCode,
+            msg: 'Failed to retrieve users'
+        })
+    }
+ })
+
+ //Add a user
+ userRouter.post('/register', bodyParser.json(), (req, res)=>{
+    try{
+        users.createUser(req, res)
+    }catch(e){
+        res.json({
+            status: res.statusCode,
+            msg: 'Failed to add a new user.'
+        })
+    }
+ })
+ export{
+    userRouter,
+    express
+ }
